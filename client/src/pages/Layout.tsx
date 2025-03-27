024651a280d933e591f1a001a4e09acb2064de46
romill-09 <romil.code09@gmail.com>
2025-03-27 08:56:52 +0530

    fix: footer positioning

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,22 +1,23 @@
 import { Outlet } from "react-router-dom";
 // import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import Navbar2 from './../components/Navbar/Navbar2';
-import { Toaster } from "@/components/ui/toaster"
-
+import Navbar2 from "./../components/Navbar/Navbar2";
+import { Toaster } from "@/components/ui/toaster";
 
 function Layout() {
   return (
     <>
       {/* <Navbar/> */}
-      <Navbar2/>
-      <div className="w-full">
-        <div className="container mx-auto">
-          <Outlet />
-          <Toaster />
+      <div className="flex flex-col min-h-screen">
+        <Navbar2 />
+        <div className="flex-grow w-full">
+          <div className="container mx-auto">
+            <Outlet />
+            <Toaster />
+          </div>
         </div>
+        <Footer />
       </div>
-      <Footer/>
     </>
   );
 }
